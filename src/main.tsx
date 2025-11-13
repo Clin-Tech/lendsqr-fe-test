071e5174835e3510dec80abaab261558f25a4581
Clinton <atayeroclinton.ac@gmail.com>
2025-11-13 10:24:11 +0100

    Fixed: MSW not starting in prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
+/// <reference types="vite/client" />
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "@/styles/index.scss";
 
-// I am starting MSW in dev only!!!!!!
-if (import.meta.env.DEV) {
-  const { worker } = await import("./mocks/browser");
-  worker.start({ onUnhandledRequest: "bypass" });
+async function bootstrap() {
+  const useMSW =
+    import.meta.env.MODE !== "test" &&
+    (import.meta.env.DEV || import.meta.env.VITE_USE_MSW === "true");
+
+  if (useMSW) {
+    const { worker } = await import("./mocks/browser");
+    await worker.start({
+      onUnhandledRequest: "bypass",
+      serviceWorker: { url: "/mockServiceWorker.js" },
+    });
+  }
+
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+bootstrap();
